0947ff692f12e0f00a0b75e0f4405900cb107437
ahmetcinaer <ahmetcinaer@gmail.com>
2022-10-11 10:58:33 +0300

    Add Odev-2

---
+++ Odev-2/Koleksiyonlar-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Koleksiyonlar_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
