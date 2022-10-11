eacaf1e92a15edff7bf8752e8c33528b5bbebdbc
ahmetcinaer <ahmetcinaer@gmail.com>
2022-10-11 12:16:52 +0300

    Add Enum

---
+++ Enum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.monday);         //monday
+            Console.WriteLine((int)Gunler.tuesday);   //2
+
+            int sicaklik = 32;
+
+            if (sicaklik <= (int)HavaDurumu.normal)
+            {
+                Console.WriteLine("you should wait for going out..");
+            }
+            else if (sicaklik >= (int)HavaDurumu.warm)
+            {
+                Console.WriteLine("the wheather is so warm for going out..");
+            }
+            else if (sicaklik >= (int)HavaDurumu.normal && sicaklik<=(int)HavaDurumu.Hot)
+            {
+                Console.WriteLine("lets go out..");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+    enum Gunler 
+    {
+        monday=1,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday
+
+    }
+
+    enum HavaDurumu
+    {
+        cold=5,
+        normal=20,
+        warm=25,
+        Hot=30
+    }
+}
